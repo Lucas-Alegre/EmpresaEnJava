147cd1f053fc9894f8b91a173b390b949e1e7e79
Lucas Alegre <lucasealegre123@gmail.com>
2022-02-16 10:21:20 -0300

    Version master

--- src/Empresa/Main.java
+++ src/Empresa/Main.java
@@ -4,7 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-
+		Usuario persona = new Usuario("Alegre", "Lucas");
+		Region localidad = new Region("Concordia, Entre ríos, Argentina");
+		Empresa empresa = new Empresa(persona, localidad);
+		
+		System.out.println("-----------------Bienvenidos------------------");
+		System.out.println(empresa.toString());
+		System.out.println("-----------------Bienvenidos------------------");
 	}
 
 }
