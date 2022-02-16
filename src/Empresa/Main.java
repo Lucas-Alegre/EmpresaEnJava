d70febe07c3884121c8252960552d2f615d77f1c
Lucas Alegre <lucasealegre123@gmail.com>
2022-02-16 11:01:29 -0300

    Version con Region

--- src/Empresa/Main.java
+++ src/Empresa/Main.java
@@ -62,4 +62,20 @@ class Usuario {
 	}
 	
 }
+class Region {
 
+	private String localidad;
+
+	public Region(String localidad) {
+		this.localidad = localidad;
+	}
+
+	public String getLocalidad() {
+		return localidad;
+	}
+	@Override
+	public String toString() {
+		return "Region [localidad=" + localidad + "]";
+	}
+
+}
